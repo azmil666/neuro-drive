d9a4e57c7345da4715e8de5772c328b05aaff3e5
Azmil Mohammed <azmilmohammed369@gmail.com>
2026-04-13 19:45:24 +0530

    sensor detection

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ canvas.width=200;
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2,canvas.width*0.9);
 const car = new Car(road.getLaneCentre(1),100,20,50);
-car.draw(ctx);
+
 
 animate();
 
 function animate(){
-    car.update();
+    car.update(road.borders);
     canvas.height=window.innerHeight;
 
     ctx.save();
